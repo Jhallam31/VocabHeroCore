7ed5874bc159b9a5962204406a59ea071b35e1c6
Jhallam31 <hallam8492@gmail.com>
2022-05-09 16:49:50 -0400

    first commit

---
+++ VocabHero.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VocabHero.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ VocabHero.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VocabHero.Models
+{
+    public class Class1
+    {
+    }
+}
